005d11f92c1f626a1196f7f6c0eee706896a1507
G RaviTeja <rockyraviteja@gmail.com>
2018-10-08 20:05:04 +0800

    First commit
    latest code

---
+++ GRTapp/GRTapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GRTapp
+//
+//  Created by G RaviTeja on 10/8/18.
+//  Copyright © 2018 GRT Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
